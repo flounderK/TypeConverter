2a8a72069419b5eb0862c9c51c9ef11afb935a26
Clif Wolfe <clifwolfe1@gmail.com>
2021-06-26 09:10:01 -0400

    Added U8 and S8 to Typeconverter

--- typeconverter.py
+++ typeconverter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from ctypes import c_uint16, c_int16, c_uint32, c_int32, c_uint64, c_int64, c_double, c_float, Structure, Union
+from ctypes import c_uint8, c_int8, c_uint16, c_int16, c_uint32, c_int32, c_uint64, c_int64, c_double, c_float, Structure, Union
 import argparse
 import re
 import sys
@@ -11,13 +11,25 @@ class TypeConverter(Union):
         def __repr__(self):
             return " ".join(["%s: %#x" % (k, getattr(self, k)) for k, v in self._fields_])
 
-    class U32(Structure, NiceFieldRepr):
-        _fields_ = [("l", c_uint32),
-                    ("h", c_uint32)]
+    class U8(Structure, NiceFieldRepr):
+        _fields_ = [("lll", c_uint8),
+                    ("llh", c_uint8),
+                    ("lhl", c_uint8),
+                    ("lhh", c_uint8),
+                    ("hll", c_uint8),
+                    ("hlh", c_uint8),
+                    ("hhl", c_uint8),
+                    ("hhh", c_uint8)]
 
-    class S32(Structure, NiceFieldRepr):
-        _fields_ = [("l", c_int32),
-                    ("h", c_int32)]
+    class S8(Structure, NiceFieldRepr):
+        _fields_ = [("lll", c_int8),
+                    ("llh", c_int8),
+                    ("lhl", c_int8),
+                    ("lhh", c_int8),
+                    ("hll", c_int8),
+                    ("hlh", c_int8),
+                    ("hhl", c_int8),
+                    ("hhh", c_int8)]
 
     class U16(Structure, NiceFieldRepr):
         _fields_ = [("ll", c_uint16),
@@ -31,6 +43,14 @@ class TypeConverter(Union):
                     ("hl", c_int16),
                     ("hh", c_int16)]
 
+    class U32(Structure, NiceFieldRepr):
+        _fields_ = [("l", c_uint32),
+                    ("h", c_uint32)]
+
+    class S32(Structure, NiceFieldRepr):
+        _fields_ = [("l", c_int32),
+                    ("h", c_int32)]
+
     class F32(Structure):
         _fields_ = [("l", c_float),
                     ("h", c_float)]
@@ -38,7 +58,9 @@ class TypeConverter(Union):
         def __repr__(self):
             return " ".join(["%s: %s" % (k, getattr(self, k)) for k, v in self._fields_])
 
-    _fields_ = [("u16", U16),
+    _fields_ = [("u8", U8),
+                ("s8", S8),
+                ("u16", U16),
                 ("s16", S16),
                 ("u32", U32),
                 ("s32", S32),
